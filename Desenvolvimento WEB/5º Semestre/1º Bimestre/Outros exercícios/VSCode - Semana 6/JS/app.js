266cbc71e05640138bc6b64db391ac08c2a03950
MMCAC <43833424+MMCAC@users.noreply.github.com>
2023-03-12 14:02:31 -0400

    add de outros files

---
+++ Desenvolvimento WEB/5º Semestre/1º Bimestre/Outros exercícios/VSCode - Semana 6/JS/app.js
@@ -0,0 +1,10 @@
+window.alert("Welcome!");
+
+function minhaFuncao(){
+    alert("Obrigado por visitar o site!");
+    document.getElementById("button").innerHTML = "?";
+}
+
+document.write("<h1>Nada por aqui!</h1>");
+
+console.log("Hello");
